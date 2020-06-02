6aeaa360cfe76267b2a09d903ad8b94310784684
John Binze Escol <johnbinzeescol@live.com>
2020-06-02 12:43:44 +0800

    Changes and additions.

---
+++ Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(184, 175)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(60, 60)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 10
+        '
+        'Timer2
+        '
+        Me.Timer2.Interval = 5
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 1
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.AboutToolStripMenuItem, Me.ExitToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(430, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(12, 27)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(406, 383)
+        Me.Panel1.TabIndex = 2
+        '
+        'AboutToolStripMenuItem
+        '
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(52, 20)
+        Me.AboutToolStripMenuItem.Text = "About"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(430, 422)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Click the Ball"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.Panel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,155 @@
+﻿Public Class Form1
+
+    Dim s As Integer = 5
+    Dim m As Integer = 5
+
+    Dim level As Integer = 1
+
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        If level = 1 Then
+            Timer1.Enabled = False
+            Dim result As DialogResult = MessageBox.Show("Good Job. Ready for level 2?",
+                                                         "Congratulations",
+                                                         MessageBoxButtons.YesNo)
+
+            If result = DialogResult.Yes Then
+                Timer2.Enabled = True
+                level += 1
+            Else
+                MessageBox.Show("Thank You for playing our game." _
+                                + Environment.NewLine +
+                                "Now closing the game",
+                                "Thank You")
+                End
+            End If
+        ElseIf level = 2 Then
+            Timer2.Enabled = False
+
+            Dim result As DialogResult = MessageBox.Show("Good Job. Ready for level 3?",
+                                                         "Congratulations",
+                                                         MessageBoxButtons.YesNo)
+
+            If result = DialogResult.Yes Then
+                Timer3.Enabled = True
+                level += 1
+            Else
+                MessageBox.Show("Thank You for playing our game." _
+                                + Environment.NewLine +
+                                "Now closing the game",
+                                "Thank You")
+                End
+            End If
+        ElseIf level = 3 Then
+            Timer3.Enabled = False
+
+            Dim result As DialogResult = MessageBox.Show("You have beaten the game. Play Again?",
+                                                         "Congratulations",
+                                                          MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                Timer1.Enabled = True
+                level = 1
+            Else
+                MessageBox.Show("Thank You for playing our game." _
+                                + Environment.NewLine +
+                                "Now closing the game",
+                                "Thank You")
+                End
+            End If
+        End If
+
+
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        PictureBox1.Location = New Point(PictureBox1.Location.X + m, PictureBox1.Location.Y + s)
+
+        Dim upperbound As Integer = 8
+        Dim lowerbound As Integer = 3
+        Dim neglowerbound As Integer = -3
+        Dim negupperbound As Integer = -8
+        Randomize()
+
+        If PictureBox1.Location.X > Panel1.Width - 60 Then
+            m = CInt(Math.Floor((negupperbound - neglowerbound + 1) * Rnd())) + neglowerbound
+        End If
+        If PictureBox1.Location.X < 0 Then
+            m = CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
+        End If
+        If PictureBox1.Location.Y > Panel1.Height - 85 Then
+            s = CInt(Math.Floor((negupperbound - neglowerbound + 1) * Rnd())) + neglowerbound
+        End If
+        If PictureBox1.Location.Y < 0 Then
+            s = CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
+        End If
+        'PictureBox1.Top += speedV
+        'PictureBox1.Left += speedH
+
+        'If PictureBox1.Top <= 0 Then
+        '    speedV = -speedV
+        'ElseIf PictureBox1.Top >= Me.ClientSize.Height - Picturebox1.height Then
+        '    speedV = -speedV
+        'End If
+
+        'If PictureBox1.Left <= 0 Then
+        '    speedH = -speedH
+        'ElseIf PictureBox1.Left >= Me.ClientSize.Width - PictureBox1.Width Then
+        '    speedH = -speedH
+        'End If
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        PictureBox1.Location = New Point(PictureBox1.Location.X + m, PictureBox1.Location.Y + s)
+
+        Dim upperbound As Integer = 10
+        Dim lowerbound As Integer = 5
+        Dim neglowerbound As Integer = -10
+        Dim negupperbound As Integer = -5
+        Randomize()
+
+        If PictureBox1.Location.X > Panel1.Width - 60 Then
+            m = CInt(Math.Floor((negupperbound - neglowerbound + 1) * Rnd())) + neglowerbound
+        End If
+        If PictureBox1.Location.X < 0 Then
+            m = CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
+        End If
+        If PictureBox1.Location.Y > Panel1.Height - 85 Then
+            s = CInt(Math.Floor((negupperbound - neglowerbound + 1) * Rnd())) + neglowerbound
+        End If
+        If PictureBox1.Location.Y < 0 Then
+            s = CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
+        End If
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        PictureBox1.Location = New Point(PictureBox1.Location.X + m, PictureBox1.Location.Y + s)
+
+        Dim upperbound As Integer = 12
+        Dim lowerbound As Integer = 7
+        Dim neglowerbound As Integer = -12
+        Dim negupperbound As Integer = -7
+        Randomize()
+
+        If PictureBox1.Location.X > Panel1.Width - 60 Then
+            m = CInt(Math.Floor((negupperbound - neglowerbound + 1) * Rnd())) + neglowerbound
+        End If
+        If PictureBox1.Location.X < 0 Then
+            m = CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
+        End If
+        If PictureBox1.Location.Y > Panel1.Height - 85 Then
+            s = CInt(Math.Floor((negupperbound - neglowerbound + 1) * Rnd())) + neglowerbound
+        End If
+        If PictureBox1.Location.Y < 0 Then
+            s = CInt(Math.Floor((upperbound - lowerbound + 1) * Rnd())) + lowerbound
+        End If
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        End
+    End Sub
+
+    Private Sub AboutToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AboutToolStripMenuItem.Click
+        Me.Enabled = False
+        Me.Hide()
+        Form2.Show()
+    End Sub
+End Class
